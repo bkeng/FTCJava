f3a61d9f065a01a1fbe125fd3fceef0cc3b206cd
Ban Seng <banseng.keng@watchguard.com>
2023-08-25 15:36:40 -0700

    show data.

--- TeamCode/src/main/java/HelloWorld.java
+++ TeamCode/src/main/java/HelloWorld.java
@@ -1,3 +1,14 @@
-public class HelloWorld {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("This","test");
+    }
 }
